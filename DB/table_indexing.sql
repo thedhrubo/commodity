b3d884e4664b54669dabe96b165c47a0a4d1af76
Shaheen Alam <shaheen@4axiz.com>
2018-10-17 11:55:45 +0600

    Added all the indexing for all tables

---
+++ DB/table_indexing.sql
@@ -0,0 +1,79 @@
+ALTER TABLE `sugar` ADD INDEX `sugar_open_price` (`open_price`);
+ALTER TABLE `sugar` ADD INDEX `sugar_highest_price` (`highest_price`);
+ALTER TABLE `sugar` ADD INDEX `sugar_lowest_price` (`lowest_price`);
+ALTER TABLE `sugar` ADD INDEX `sugar_closed_price` (`closed_price`);
+ALTER TABLE `sugar` ADD INDEX `sugar_stockdate` (`stockdate`);
+ALTER TABLE `sugar` ADD INDEX `sugar_sellyear` (`sellyear`);
+ALTER TABLE `sugar` ADD INDEX `sugar_sellmonth` (`sellmonth`);
+
+ALTER TABLE `cocoa` ADD INDEX `cocoa_open_price` (`open_price`);
+ALTER TABLE `cocoa` ADD INDEX `cocoa_highest_price` (`highest_price`);
+ALTER TABLE `cocoa` ADD INDEX `cocoa_lowest_price` (`lowest_price`);
+ALTER TABLE `cocoa` ADD INDEX `cocoa_closed_price` (`closed_price`);
+ALTER TABLE `cocoa` ADD INDEX `cocoa_stockdate` (`stockdate`);
+ALTER TABLE `cocoa` ADD INDEX `cocoa_sellyear` (`sellyear`);
+ALTER TABLE `cocoa` ADD INDEX `cocoa_sellmonth` (`sellmonth`);
+
+ALTER TABLE `coffee` ADD INDEX `coffee_open_price` (`open_price`);
+ALTER TABLE `coffee` ADD INDEX `coffee_highest_price` (`highest_price`);
+ALTER TABLE `coffee` ADD INDEX `coffee_lowest_price` (`lowest_price`);
+ALTER TABLE `coffee` ADD INDEX `coffee_closed_price` (`closed_price`);
+ALTER TABLE `coffee` ADD INDEX `coffee_stockdate` (`stockdate`);
+ALTER TABLE `coffee` ADD INDEX `coffee_sellyear` (`sellyear`);
+ALTER TABLE `coffee` ADD INDEX `coffee_sellmonth` (`sellmonth`);
+
+ALTER TABLE `corn` ADD INDEX `corn_open_price` (`open_price`);
+ALTER TABLE `corn` ADD INDEX `corn_highest_price` (`highest_price`);
+ALTER TABLE `corn` ADD INDEX `corn_lowest_price` (`lowest_price`);
+ALTER TABLE `corn` ADD INDEX `corn_closed_price` (`closed_price`);
+ALTER TABLE `corn` ADD INDEX `corn_stockdate` (`stockdate`);
+ALTER TABLE `corn` ADD INDEX `corn_sellyear` (`sellyear`);
+ALTER TABLE `corn` ADD INDEX `corn_sellmonth` (`sellmonth`);
+
+ALTER TABLE `gold` ADD INDEX `gold_open_price` (`open_price`);
+ALTER TABLE `gold` ADD INDEX `gold_highest_price` (`highest_price`);
+ALTER TABLE `gold` ADD INDEX `gold_lowest_price` (`lowest_price`);
+ALTER TABLE `gold` ADD INDEX `gold_closed_price` (`closed_price`);
+ALTER TABLE `gold` ADD INDEX `gold_stockdate` (`stockdate`);
+ALTER TABLE `gold` ADD INDEX `gold_sellyear` (`sellyear`);
+ALTER TABLE `gold` ADD INDEX `gold_sellmonth` (`sellmonth`);
+
+ALTER TABLE `live_cattle` ADD INDEX `live_cattle_open_price` (`open_price`);
+ALTER TABLE `live_cattle` ADD INDEX `live_cattle_highest_price` (`highest_price`);
+ALTER TABLE `live_cattle` ADD INDEX `live_cattle_lowest_price` (`lowest_price`);
+ALTER TABLE `live_cattle` ADD INDEX `live_cattle_closed_price` (`closed_price`);
+ALTER TABLE `live_cattle` ADD INDEX `live_cattle_stockdate` (`stockdate`);
+ALTER TABLE `live_cattle` ADD INDEX `live_cattle_sellyear` (`sellyear`);
+ALTER TABLE `live_cattle` ADD INDEX `live_cattle_sellmonth` (`sellmonth`);
+
+ALTER TABLE `oil` ADD INDEX `oil_open_price` (`open_price`);
+ALTER TABLE `oil` ADD INDEX `oil_highest_price` (`highest_price`);
+ALTER TABLE `oil` ADD INDEX `oil_lowest_price` (`lowest_price`);
+ALTER TABLE `oil` ADD INDEX `oil_closed_price` (`closed_price`);
+ALTER TABLE `oil` ADD INDEX `oil_stockdate` (`stockdate`);
+ALTER TABLE `oil` ADD INDEX `oil_sellyear` (`sellyear`);
+ALTER TABLE `oil` ADD INDEX `oil_sellmonth` (`sellmonth`);
+
+ALTER TABLE `platinum` ADD INDEX `platinum_open_price` (`open_price`);
+ALTER TABLE `platinum` ADD INDEX `platinum_highest_price` (`highest_price`);
+ALTER TABLE `platinum` ADD INDEX `platinum_lowest_price` (`lowest_price`);
+ALTER TABLE `platinum` ADD INDEX `platinum_closed_price` (`closed_price`);
+ALTER TABLE `platinum` ADD INDEX `platinum_stockdate` (`stockdate`);
+ALTER TABLE `platinum` ADD INDEX `platinum_sellyear` (`sellyear`);
+ALTER TABLE `platinum` ADD INDEX `platinum_sellmonth` (`sellmonth`);
+
+ALTER TABLE `silver` ADD INDEX `silver_open_price` (`open_price`);
+ALTER TABLE `silver` ADD INDEX `silver_highest_price` (`highest_price`);
+ALTER TABLE `silver` ADD INDEX `silver_lowest_price` (`lowest_price`);
+ALTER TABLE `silver` ADD INDEX `silver_closed_price` (`closed_price`);
+ALTER TABLE `silver` ADD INDEX `silver_stockdate` (`stockdate`);
+ALTER TABLE `silver` ADD INDEX `silver_sellyear` (`sellyear`);
+ALTER TABLE `silver` ADD INDEX `silver_sellmonth` (`sellmonth`);
+
+ALTER TABLE `wheat` ADD INDEX `wheat_open_price` (`open_price`);
+ALTER TABLE `wheat` ADD INDEX `wheat_highest_price` (`highest_price`);
+ALTER TABLE `wheat` ADD INDEX `wheat_lowest_price` (`lowest_price`);
+ALTER TABLE `wheat` ADD INDEX `wheat_closed_price` (`closed_price`);
+ALTER TABLE `wheat` ADD INDEX `wheat_stockdate` (`stockdate`);
+ALTER TABLE `wheat` ADD INDEX `wheat_sellyear` (`sellyear`);
+ALTER TABLE `wheat` ADD INDEX `wheat_sellmonth` (`sellmonth`);
